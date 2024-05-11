94230cdaf7517d256563510d14ddcc81101f5dda
Anand Kumar Rohal <anandrohal4595@gmail.com>
2024-05-11 11:37:01 +0530

    feature/001 First commit

---
+++ src/main/java/com/example/Book/Store/API/BookStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Book.Store.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Book/Store/API/BookStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Book.Store.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
